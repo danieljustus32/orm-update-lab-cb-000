7214d0c1d3c2e5a3bfa41248f2a0ae7323be56ec
Daniel Justus <daniel.justus32@gmail.com>
2017-12-07 00:29:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,13 +44,13 @@ class Student
   end
 
   def self.create(name, grade)
-    student = self.new(name, grade, id = nil)
+    student = self.new(name, grade)
     student.save
     student
   end
 
   def self.new_from_db(row)
-    self.create(row[1], row[2])
+    self.create(row[1], row[2], id = row[0])
   end
 
   def self.find_by_name(name)
